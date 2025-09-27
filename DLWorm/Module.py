ed9e18f244d4569ac83b5ad65eb19638cdd7c5e7
Vitriol-nT <161344449+Vitriol-nT@users.noreply.github.com>
2025-09-27 11:24:47 +0900

    Update Module.py

--- DLWorm/Module.py
+++ DLWorm/Module.py
@@ -27,52 +27,49 @@ class Worm:
         self.End = True
 
     def moving(self, input):
-        DirectionTrue = [False, False, False, False]
-        #East, West, South, North
-        #setting the facing side
-        #cannot go through previous move selections
-        if input == "u" and DirectionTrue[2] == False:
+        nx, ny = self.pointx, self.pointy
+        if input == "u":
             self.facing = "north"
-            if place[self.pointy - 1][self.pointx] == 1:
-                self.death()
-            else:
-                self.pointy -= 1
-            DirectionTrue = [False, False, False, True]
-        elif input == "d" and DirectionTrue[3] == False:
+            nx, ny = self.pointx, self.pointy - 1
+        elif input == "d":
             self.facing = "south"
-            if place[self.pointy + 1][self.pointx] == 1:
-                self.death()
-            else:
-                self.pointy += 1
-            DirectionTrue = [False, False, True, False]
-        elif input == "l" and DirectionTrue[0] == False:
+            nx, ny = self.pointx, self.pointy + 1
+        elif input == "l":
             self.facing = "west"
-            if place[self.pointy][self.pointx - 1] == 1:
-                self.death()
-            else:
-                self.pointx -= 1
-            DirectionTrue = [False, True, False, False]
-        elif input == "r" and DirectionTrue[1] == False:
+            nx, ny = self.pointx - 1, self.pointy
+        elif input == "r":
             self.facing = "east"
-            if place[self.pointy][self.pointx + 1] == 1:
-                self.death()
-            else:
-                self.pointx += 1
-            DirectionTrue = [True, False, False, False]
-        #at default, will be moving 1 pixel per .5 seconds for the heading direction
+            nx, ny = self.pointx + 1, self.pointy
 
-        if self.pointy < 0 or self.pointy > 19 or self.pointx < 0 or self.pointx > 19:
+        if nx < 0 or nx >= GRIDSIZE or ny < 0 or ny >= GRIDSIZE:
             self.death()
-
+            return
+            
+        if place[ny][nx] == 1:
+            self.death()
+            return
+            
+        self.pointx, self.pointy = nx, ny
     def drawing(self):
-        #The display will be on a web application. Only code for value change 0 to 1
-        #move the head first, then rest will be tracing the parts
         self.historyx.append(self.pointx)
         self.historyy.append(self.pointy)
-        
-        for i in range(self.length):
-            place[self.historyy[len(self.historyy) - 1 - i]][self.historyx[len(self.historyx) - 1 - i]] = 1
-            place[self.historyy[len(self.historyy) - self.length - 1]][self.historyx[len(self.historyx) - self.length - 1]] = 0
+
+        hist_len = len(self.historyx)
+        n_body = min(self.length, hist_len)
+        for i in range(n_body):
+            hx = self.historyx[hist_len - 1 - i]
+            hy = self.historyy[hist_len - 1 - i]
+            place[hy][hx] = 1
+
+        place[self.pointy][self.pointx] = 3
+
+        if hist_len > self.length:
+            tail_i = hist_len - self.length - 1
+            tx = self.historyx[tail_i]
+            ty = self.historyy[tail_i]
+            # extra safety: only clear if indices valid
+            if 0 <= tx < GRIDSIZE and 0 <= ty < GRIDSIZE:
+                place[ty][tx] = 0
 
 class food:
     def __init__(self, pointxf, pointyf):
@@ -104,6 +101,8 @@ class ActionSpace:
             def sample(self):
                 return random.randint(0, self.n - 1)
 
+reward = 0
+
 class WormEnv:
     def __init__(self):
         self.action_space = type('', (), {})()
@@ -113,41 +112,52 @@ class WormEnv:
         self.reset()
         self.worm = Worm()
     
+    def _get_state(self):
+        return np.array(place, dtype=np.float32).flatten()
+
     def reset(self):
         global place
+        global reward
         place = [[0 for _ in range(GRIDSIZE)] for _ in range(GRIDSIZE)]
         self.worm = Worm(5, 5)
         self.food1 = food(10, 10)
         self.food1.placement()
-        self.food2 = food(5, 6)
+        self.food2 = food(5, 7)
         self.food2.placement()
         self.steps = 0
+        self.worm.length = 5
         for _ in range(self.worm.length):
             self.worm.historyy.append(self.worm.pointy)
             self.worm.historyx.append(self.worm.pointx + 4 - _)
         state = self._get_state()
+        reward = 0
         return state, {}
     
     def step(self, action):
+        global reward
         self.worm.moving(action)
-        self.food1.eat(self.worm)
-        self.food2.eat(self.worm)
-        self.worm.drawing()
+        print(action)
         self.steps += 1
-
-        reward = 0.1 if not self.worm.End else - 10
+        print(f"pos({self.worm.pointx},{self.worm.pointy})")
+        reward += 0.01
         if self.worm.pointx == self.food1.pointxf and self.worm.pointy == self.food1.pointyf:
-            reward += 10
+            self.food1.eat(self.worm)
+            reward += 50
+            self.steps -= 10
         if self.worm.pointx == self.food2.pointxf and self.worm.pointy == self.food2.pointyf:
-            reward += 10
+            self.food2.eat(self.worm)
+            reward += 50
+            self.steps -= 10
         
-        done = self.worm.End or self.steps > 300
-
+        self.worm.drawing()
+        done = self.worm.End or self.steps > 200
+        if done:
+            reward -= 250
+            print(f"Got score:{self.food1.score + self.food2.score}")
+            print(f"with reward of: {reward}")
+            print(place)
         state = self._get_state()
         return state, reward, done, False, {}
-
-    def _get_state(self):
-        return np.array(place, dtype=np.float32).flatten()
     
     def render(self):
         for row in place:
